8ee29ffee8e3b41c00132a02488de88b8d6ba229
Matthew Freedman <CanMatthewCode@gmail.com>
2025-10-22 14:52:55 -0700

    add architectsLog_constants.py with PHASES dictionary

---
+++ src/architectsLog_constants.py
@@ -0,0 +1,13 @@
+#Constants for the Architect's Log
+
+#All possible phases eachg project will go through
+PHASES = {
+	1: "Scematic Design", 
+	2: "Design Development", 
+	3: "Construction Documents", 
+	4: "Bidding Negotiation", 
+	5: "Construction Administration",
+	6: "Interior Design"
+	7: "Business Development"
+	8: "Administration"
+	}

99630e2837afb65f898bb0947f39535318ac29a0
Matthew Freedman <CanMatthewCode@gmail.com>
2025-12-15 09:31:21 -0800

    update MainWindow.ui/.py layout, add ViewArchitects.ui/.py with 'Hide Inactive' checkbox and QTableView to show all architects

--- src/ui/MainWindow.py
+++ src/ui/MainWindow.py
@@ -24,7 +24,7 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(907, 479)
+        MainWindow.resize(900, 480)
         sizePolicy = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -37,13 +37,11 @@ class Ui_MainWindow(object):
         self.centralwidget.setStyleSheet(u"QWidget{\n"
 "	background-color: #1E2E34;\n"
 "}")
-        self.layoutWidget = QWidget(self.centralwidget)
-        self.layoutWidget.setObjectName(u"layoutWidget")
-        self.layoutWidget.setGeometry(QRect(0, 0, 901, 421))
-        self.main_layout = QVBoxLayout(self.layoutWidget)
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.main_layout = QVBoxLayout()
         self.main_layout.setObjectName(u"main_layout")
-        self.main_layout.setContentsMargins(0, 0, 0, 0)
-        self.MainTitle = QLabel(self.layoutWidget)
+        self.MainTitle = QLabel(self.centralwidget)
         self.MainTitle.setObjectName(u"MainTitle")
         self.MainTitle.setMinimumSize(QSize(501, 61))
         font = QFont()
@@ -62,21 +60,20 @@ class Ui_MainWindow(object):
         self.project_button_layout.setObjectName(u"project_button_layout")
         self.architects_layout = QVBoxLayout()
         self.architects_layout.setObjectName(u"architects_layout")
-        self.Architects = QLabel(self.layoutWidget)
+        self.Architects = QLabel(self.centralwidget)
         self.Architects.setObjectName(u"Architects")
         font1 = QFont()
         font1.setPointSize(24)
         font1.setBold(True)
         self.Architects.setFont(font1)
         self.Architects.setStyleSheet(u"QLabel{\n"
-"\n"
 "	color: #89D5D2;\n"
 "}")
         self.Architects.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.architects_layout.addWidget(self.Architects)
 
-        self.ArchitectsComboBox = QComboBox(self.layoutWidget)
+        self.ArchitectsComboBox = QComboBox(self.centralwidget)
         self.ArchitectsComboBox.setObjectName(u"ArchitectsComboBox")
         sizePolicy.setHeightForWidth(self.ArchitectsComboBox.sizePolicy().hasHeightForWidth())
         self.ArchitectsComboBox.setSizePolicy(sizePolicy)
@@ -87,7 +84,7 @@ class Ui_MainWindow(object):
 
         self.architects_layout.addWidget(self.ArchitectsComboBox, 0, Qt.AlignmentFlag.AlignVCenter)
 
-        self.AddArchitectBtn = QPushButton(self.layoutWidget)
+        self.AddArchitectBtn = QPushButton(self.centralwidget)
         self.AddArchitectBtn.setObjectName(u"AddArchitectBtn")
         sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
         sizePolicy1.setHorizontalStretch(20)
@@ -122,7 +119,7 @@ class Ui_MainWindow(object):
 
         self.projects_layout = QVBoxLayout()
         self.projects_layout.setObjectName(u"projects_layout")
-        self.Projects = QLabel(self.layoutWidget)
+        self.Projects = QLabel(self.centralwidget)
         self.Projects.setObjectName(u"Projects")
         self.Projects.setFont(font1)
         self.Projects.setStyleSheet(u"QLabel{\n"
@@ -132,7 +129,7 @@ class Ui_MainWindow(object):
 
         self.projects_layout.addWidget(self.Projects, 0, Qt.AlignmentFlag.AlignHCenter|Qt.AlignmentFlag.AlignVCenter)
 
-        self.ProjectsComboBox = QComboBox(self.layoutWidget)
+        self.ProjectsComboBox = QComboBox(self.centralwidget)
         self.ProjectsComboBox.setObjectName(u"ProjectsComboBox")
         sizePolicy.setHeightForWidth(self.ProjectsComboBox.sizePolicy().hasHeightForWidth())
         self.ProjectsComboBox.setSizePolicy(sizePolicy)
@@ -140,7 +137,7 @@ class Ui_MainWindow(object):
 
         self.projects_layout.addWidget(self.ProjectsComboBox, 0, Qt.AlignmentFlag.AlignVCenter)
 
-        self.AddProjectBtn = QPushButton(self.layoutWidget)
+        self.AddProjectBtn = QPushButton(self.centralwidget)
         self.AddProjectBtn.setObjectName(u"AddProjectBtn")
         sizePolicy1.setHeightForWidth(self.AddProjectBtn.sizePolicy().hasHeightForWidth())
         self.AddProjectBtn.setSizePolicy(sizePolicy1)
@@ -171,7 +168,7 @@ class Ui_MainWindow(object):
         self.phases_layout = QVBoxLayout()
         self.phases_layout.setObjectName(u"phases_layout")
         self.phases_layout.setSizeConstraint(QLayout.SizeConstraint.SetDefaultConstraint)
-        self.ProjectPhases = QLabel(self.layoutWidget)
+        self.ProjectPhases = QLabel(self.centralwidget)
         self.ProjectPhases.setObjectName(u"ProjectPhases")
         self.ProjectPhases.setMaximumSize(QSize(16777215, 16777215))
         self.ProjectPhases.setFont(font1)
@@ -182,7 +179,7 @@ class Ui_MainWindow(object):
 
         self.phases_layout.addWidget(self.ProjectPhases, 0, Qt.AlignmentFlag.AlignHCenter|Qt.AlignmentFlag.AlignTop)
 
-        self.PhasesComboBox = QComboBox(self.layoutWidget)
+        self.PhasesComboBox = QComboBox(self.centralwidget)
         self.PhasesComboBox.setObjectName(u"PhasesComboBox")
         sizePolicy.setHeightForWidth(self.PhasesComboBox.sizePolicy().hasHeightForWidth())
         self.PhasesComboBox.setSizePolicy(sizePolicy)
@@ -213,7 +210,7 @@ class Ui_MainWindow(object):
 
         self.time_button_layout.addItem(self.left_time_spacer)
 
-        self.LogTimeBtn = QPushButton(self.layoutWidget)
+        self.LogTimeBtn = QPushButton(self.centralwidget)
         self.LogTimeBtn.setObjectName(u"LogTimeBtn")
         sizePolicy2 = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
         sizePolicy2.setHorizontalStretch(20)
@@ -243,7 +240,7 @@ class Ui_MainWindow(object):
 
         self.time_button_layout.addWidget(self.LogTimeBtn)
 
-        self.AddTimeBtn = QPushButton(self.layoutWidget)
+        self.AddTimeBtn = QPushButton(self.centralwidget)
         self.AddTimeBtn.setObjectName(u"AddTimeBtn")
         sizePolicy2.setHeightForWidth(self.AddTimeBtn.sizePolicy().hasHeightForWidth())
         self.AddTimeBtn.setSizePolicy(sizePolicy2)
@@ -277,15 +274,15 @@ class Ui_MainWindow(object):
 
         self.logs_layout = QHBoxLayout()
         self.logs_layout.setObjectName(u"logs_layout")
-        self.ArchitectLogsBtn = QPushButton(self.layoutWidget)
-        self.ArchitectLogsBtn.setObjectName(u"ArchitectLogsBtn")
-        sizePolicy.setHeightForWidth(self.ArchitectLogsBtn.sizePolicy().hasHeightForWidth())
-        self.ArchitectLogsBtn.setSizePolicy(sizePolicy)
-        self.ArchitectLogsBtn.setMinimumSize(QSize(150, 50))
-        self.ArchitectLogsBtn.setMaximumSize(QSize(16777215, 16777215))
-        self.ArchitectLogsBtn.setFont(font2)
-        self.ArchitectLogsBtn.setLayoutDirection(Qt.LayoutDirection.LeftToRight)
-        self.ArchitectLogsBtn.setStyleSheet(u"QPushButton{\n"
+        self.ViewArchitectsBtn = QPushButton(self.centralwidget)
+        self.ViewArchitectsBtn.setObjectName(u"ViewArchitectsBtn")
+        sizePolicy.setHeightForWidth(self.ViewArchitectsBtn.sizePolicy().hasHeightForWidth())
+        self.ViewArchitectsBtn.setSizePolicy(sizePolicy)
+        self.ViewArchitectsBtn.setMinimumSize(QSize(150, 50))
+        self.ViewArchitectsBtn.setMaximumSize(QSize(16777215, 16777215))
+        self.ViewArchitectsBtn.setFont(font2)
+        self.ViewArchitectsBtn.setLayoutDirection(Qt.LayoutDirection.LeftToRight)
+        self.ViewArchitectsBtn.setStyleSheet(u"QPushButton{\n"
 "	border-radius: 12px;\n"
 "	border: 1px solid black;\n"
 "	background-color: #008080;\n"
@@ -299,20 +296,20 @@ class Ui_MainWindow(object):
 "QPushButton:pressed {\n"
 "	background-color: #004D4D;\n"
 "}")
-        self.ArchitectLogsBtn.setIconSize(QSize(10, 16))
-        self.ArchitectLogsBtn.setAutoDefault(True)
+        self.ViewArchitectsBtn.setIconSize(QSize(10, 16))
+        self.ViewArchitectsBtn.setAutoDefault(True)
 
-        self.logs_layout.addWidget(self.ArchitectLogsBtn, 0, Qt.AlignmentFlag.AlignHCenter|Qt.AlignmentFlag.AlignVCenter)
+        self.logs_layout.addWidget(self.ViewArchitectsBtn, 0, Qt.AlignmentFlag.AlignHCenter|Qt.AlignmentFlag.AlignVCenter)
 
-        self.ProjectLogsBtn = QPushButton(self.layoutWidget)
-        self.ProjectLogsBtn.setObjectName(u"ProjectLogsBtn")
-        sizePolicy.setHeightForWidth(self.ProjectLogsBtn.sizePolicy().hasHeightForWidth())
-        self.ProjectLogsBtn.setSizePolicy(sizePolicy)
-        self.ProjectLogsBtn.setMinimumSize(QSize(150, 50))
-        self.ProjectLogsBtn.setMaximumSize(QSize(16777215, 16777215))
-        self.ProjectLogsBtn.setFont(font2)
-        self.ProjectLogsBtn.setLayoutDirection(Qt.LayoutDirection.LeftToRight)
-        self.ProjectLogsBtn.setStyleSheet(u"QPushButton{\n"
+        self.ViewProjectsBtn = QPushButton(self.centralwidget)
+        self.ViewProjectsBtn.setObjectName(u"ViewProjectsBtn")
+        sizePolicy.setHeightForWidth(self.ViewProjectsBtn.sizePolicy().hasHeightForWidth())
+        self.ViewProjectsBtn.setSizePolicy(sizePolicy)
+        self.ViewProjectsBtn.setMinimumSize(QSize(150, 50))
+        self.ViewProjectsBtn.setMaximumSize(QSize(16777215, 16777215))
+        self.ViewProjectsBtn.setFont(font2)
+        self.ViewProjectsBtn.setLayoutDirection(Qt.LayoutDirection.LeftToRight)
+        self.ViewProjectsBtn.setStyleSheet(u"QPushButton{\n"
 "	border-radius: 12px;\n"
 "	border: 1px solid black;\n"
 "	background-color: #008080;\n"
@@ -326,20 +323,20 @@ class Ui_MainWindow(object):
 "QPushButton:pressed {\n"
 "	background-color: #004D4D;\n"
 "}")
-        self.ProjectLogsBtn.setIconSize(QSize(10, 16))
-        self.ProjectLogsBtn.setAutoDefault(True)
+        self.ViewProjectsBtn.setIconSize(QSize(10, 16))
+        self.ViewProjectsBtn.setAutoDefault(True)
 
-        self.logs_layout.addWidget(self.ProjectLogsBtn, 0, Qt.AlignmentFlag.AlignHCenter|Qt.AlignmentFlag.AlignVCenter)
+        self.logs_layout.addWidget(self.ViewProjectsBtn, 0, Qt.AlignmentFlag.AlignHCenter|Qt.AlignmentFlag.AlignVCenter)
 
-        self.TimeLogsBtn = QPushButton(self.layoutWidget)
-        self.TimeLogsBtn.setObjectName(u"TimeLogsBtn")
-        sizePolicy.setHeightForWidth(self.TimeLogsBtn.sizePolicy().hasHeightForWidth())
-        self.TimeLogsBtn.setSizePolicy(sizePolicy)
-        self.TimeLogsBtn.setMinimumSize(QSize(150, 50))
-        self.TimeLogsBtn.setMaximumSize(QSize(16777215, 16777215))
-        self.TimeLogsBtn.setFont(font2)
-        self.TimeLogsBtn.setLayoutDirection(Qt.LayoutDirection.LeftToRight)
-        self.TimeLogsBtn.setStyleSheet(u"QPushButton{\n"
+        self.ViewTimeLogsBtn = QPushButton(self.centralwidget)
+        self.ViewTimeLogsBtn.setObjectName(u"ViewTimeLogsBtn")
+        sizePolicy.setHeightForWidth(self.ViewTimeLogsBtn.sizePolicy().hasHeightForWidth())
+        self.ViewTimeLogsBtn.setSizePolicy(sizePolicy)
+        self.ViewTimeLogsBtn.setMinimumSize(QSize(150, 50))
+        self.ViewTimeLogsBtn.setMaximumSize(QSize(16777215, 16777215))
+        self.ViewTimeLogsBtn.setFont(font2)
+        self.ViewTimeLogsBtn.setLayoutDirection(Qt.LayoutDirection.LeftToRight)
+        self.ViewTimeLogsBtn.setStyleSheet(u"QPushButton{\n"
 "	border-radius: 12px;\n"
 "	border: 1px solid black;\n"
 "	background-color: #008080;\n"
@@ -353,20 +350,20 @@ class Ui_MainWindow(object):
 "QPushButton:pressed {\n"
 "	background-color: #004D4D;\n"
 "}")
-        self.TimeLogsBtn.setIconSize(QSize(10, 16))
-        self.TimeLogsBtn.setAutoDefault(True)
+        self.ViewTimeLogsBtn.setIconSize(QSize(10, 16))
+        self.ViewTimeLogsBtn.setAutoDefault(True)
 
-        self.logs_layout.addWidget(self.TimeLogsBtn, 0, Qt.AlignmentFlag.AlignHCenter|Qt.AlignmentFlag.AlignVCenter)
+        self.logs_layout.addWidget(self.ViewTimeLogsBtn, 0, Qt.AlignmentFlag.AlignHCenter|Qt.AlignmentFlag.AlignVCenter)
 
-        self.InvoiceLogsBtn = QPushButton(self.layoutWidget)
-        self.InvoiceLogsBtn.setObjectName(u"InvoiceLogsBtn")
-        sizePolicy.setHeightForWidth(self.InvoiceLogsBtn.sizePolicy().hasHeightForWidth())
-        self.InvoiceLogsBtn.setSizePolicy(sizePolicy)
-        self.InvoiceLogsBtn.setMinimumSize(QSize(150, 50))
-        self.InvoiceLogsBtn.setMaximumSize(QSize(16777215, 16777215))
-        self.InvoiceLogsBtn.setFont(font2)
-        self.InvoiceLogsBtn.setLayoutDirection(Qt.LayoutDirection.LeftToRight)
-        self.InvoiceLogsBtn.setStyleSheet(u"QPushButton{\n"
+        self.ViewInvoiceLogsBtn = QPushButton(self.centralwidget)
+        self.ViewInvoiceLogsBtn.setObjectName(u"ViewInvoiceLogsBtn")
+        sizePolicy.setHeightForWidth(self.ViewInvoiceLogsBtn.sizePolicy().hasHeightForWidth())
+        self.ViewInvoiceLogsBtn.setSizePolicy(sizePolicy)
+        self.ViewInvoiceLogsBtn.setMinimumSize(QSize(150, 50))
+        self.ViewInvoiceLogsBtn.setMaximumSize(QSize(16777215, 16777215))
+        self.ViewInvoiceLogsBtn.setFont(font2)
+        self.ViewInvoiceLogsBtn.setLayoutDirection(Qt.LayoutDirection.LeftToRight)
+        self.ViewInvoiceLogsBtn.setStyleSheet(u"QPushButton{\n"
 "	border-radius: 12px;\n"
 "	border: 1px solid black;\n"
 "	background-color: #008080;\n"
@@ -380,18 +377,21 @@ class Ui_MainWindow(object):
 "QPushButton:pressed {\n"
 "	background-color: #004D4D;\n"
 "}")
-        self.InvoiceLogsBtn.setIconSize(QSize(10, 16))
-        self.InvoiceLogsBtn.setAutoDefault(True)
+        self.ViewInvoiceLogsBtn.setIconSize(QSize(10, 16))
+        self.ViewInvoiceLogsBtn.setAutoDefault(True)
 
-        self.logs_layout.addWidget(self.InvoiceLogsBtn, 0, Qt.AlignmentFlag.AlignHCenter|Qt.AlignmentFlag.AlignVCenter)
+        self.logs_layout.addWidget(self.ViewInvoiceLogsBtn, 0, Qt.AlignmentFlag.AlignHCenter|Qt.AlignmentFlag.AlignVCenter)
 
 
         self.main_layout.addLayout(self.logs_layout)
 
+
+        self.verticalLayout.addLayout(self.main_layout)
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 907, 24))
+        self.menubar.setGeometry(QRect(0, 0, 900, 24))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
@@ -412,9 +412,9 @@ class Ui_MainWindow(object):
         self.ProjectPhases.setText(QCoreApplication.translate("MainWindow", u"Project Phase", None))
         self.LogTimeBtn.setText(QCoreApplication.translate("MainWindow", u"LOG TIME", None))
         self.AddTimeBtn.setText(QCoreApplication.translate("MainWindow", u"ADD TIME", None))
-        self.ArchitectLogsBtn.setText(QCoreApplication.translate("MainWindow", u"View Architects", None))
-        self.ProjectLogsBtn.setText(QCoreApplication.translate("MainWindow", u"View Projects", None))
-        self.TimeLogsBtn.setText(QCoreApplication.translate("MainWindow", u"View Time Logs", None))
-        self.InvoiceLogsBtn.setText(QCoreApplication.translate("MainWindow", u"View Invoices", None))
+        self.ViewArchitectsBtn.setText(QCoreApplication.translate("MainWindow", u"View Architects", None))
+        self.ViewProjectsBtn.setText(QCoreApplication.translate("MainWindow", u"View Projects", None))
+        self.ViewTimeLogsBtn.setText(QCoreApplication.translate("MainWindow", u"View Time Logs", None))
+        self.ViewInvoiceLogsBtn.setText(QCoreApplication.translate("MainWindow", u"View Invoices", None))
     # retranslateUi
 
---
+++ src/ui/ViewArchitects.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'ViewArchitects.ui'
+##
+## Created by: Qt User Interface Compiler version 6.9.3
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QCheckBox, QHeaderView, QLabel,
+    QSizePolicy, QTableView, QVBoxLayout, QWidget)
+
+class Ui_ViewArchitectsWindow(object):
+    def setupUi(self, ViewArchitectsWindow):
+        if not ViewArchitectsWindow.objectName():
+            ViewArchitectsWindow.setObjectName(u"ViewArchitectsWindow")
+        ViewArchitectsWindow.resize(830, 450)
+        ViewArchitectsWindow.setMinimumSize(QSize(830, 450))
+        ViewArchitectsWindow.setStyleSheet(u"QWidget{\n"
+"	background-color: #1E2E34;\n"
+"}")
+        self.verticalLayout = QVBoxLayout(ViewArchitectsWindow)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.MainTitle = QLabel(ViewArchitectsWindow)
+        self.MainTitle.setObjectName(u"MainTitle")
+        self.MainTitle.setMinimumSize(QSize(550, 60))
+        font = QFont()
+        font.setPointSize(48)
+        font.setBold(True)
+        font.setUnderline(True)
+        self.MainTitle.setFont(font)
+        self.MainTitle.setStyleSheet(u"QLabel{\n"
+"	color: #008080;\n"
+"}")
+        self.MainTitle.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.verticalLayout.addWidget(self.MainTitle)
+
+        self.hideArchitectCheckBox = QCheckBox(ViewArchitectsWindow)
+        self.hideArchitectCheckBox.setObjectName(u"hideArchitectCheckBox")
+        self.hideArchitectCheckBox.setStyleSheet(u"QCheckBox{\n"
+"	color: #35B5AC;\n"
+"}")
+
+        self.verticalLayout.addWidget(self.hideArchitectCheckBox, 0, Qt.AlignmentFlag.AlignRight)
+
+        self.architectsTableView = QTableView(ViewArchitectsWindow)
+        self.architectsTableView.setObjectName(u"architectsTableView")
+        self.architectsTableView.setStyleSheet(u"QTableView{\n"
+"	color: #89D5D2;\n"
+"}")
+        self.architectsTableView.setAlternatingRowColors(False)
+
+        self.verticalLayout.addWidget(self.architectsTableView)
+
+
+        self.retranslateUi(ViewArchitectsWindow)
+
+        QMetaObject.connectSlotsByName(ViewArchitectsWindow)
+    # setupUi
+
+    def retranslateUi(self, ViewArchitectsWindow):
+        ViewArchitectsWindow.setWindowTitle(QCoreApplication.translate("ViewArchitectsWindow", u"Form", None))
+        self.MainTitle.setText(QCoreApplication.translate("ViewArchitectsWindow", u"Architects", None))
+        self.hideArchitectCheckBox.setText(QCoreApplication.translate("ViewArchitectsWindow", u"Hide Inactive", None))
+    # retranslateUi
+

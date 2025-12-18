918689417c8cc7c1d98fcc3c2fbb6b0a933d3991
Matthew Freedman <CanMatthewCode@gmail.com>
2025-12-18 09:39:47 -0800

    add ViewProjects and TimeLogger ui’s to file and connect them to their buttons with viewProjects and logTime methods; add applyViewState method to ArchitectWindow class to properly hide rows of inactive architects on model reset; modify table column widths when status column is visible/not visible; add ViewProjects class to display projects table     on ViewProjectsWindow’s QSqlRelationalTableModel including dataChanged method to reset PhasesComboBox on main_window     when the project’s status changes in ViewProjects table; add TimeLogger class and set combo boxes to correct tables; add setCrossComboBox and setLinkedComboBox functions to allow one combo box to change another’s displayed output

--- src/architectsLog_PySide.py
+++ src/architectsLog_PySide.py
@@ -7,13 +7,15 @@ import re
 from PySide6.QtWidgets import (QMainWindow, QApplication, QDialog, QMessageBox,
 	QStyledItemDelegate, QComboBox, QWidget)
 from PySide6.QtSql import (QSqlDatabase, QSqlTableModel, QSqlRelationalTableModel, 
-	QSqlRelation)
-from PySide6.QtCore import Qt, QDate
+	QSqlRelation, QSqlRelationalDelegate)
+from PySide6.QtCore import Qt, QDate, QModelIndex, QTimer
 
 from ui.MainWindow import Ui_MainWindow
 from ui.AddArchitect import Ui_AddArchitectDialog
 from ui.AddProject import Ui_AddProjectDialog
 from ui.ViewArchitects import Ui_ViewArchitectsWindow
+from ui.ViewProjects import Ui_ViewProjectsWindow
+from ui.TimeLogger import Ui_TimeLoggerWindow
 
 from architectsLog_classes import Architect, Project, Invoice, TimeEntry 
 from architectsLog_constants import	(PHASES, ARCHITECT_STATUSES, PROJECT_STATUSES, 
@@ -39,7 +41,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 		self.architect_model.setTable("architects")
 		self.architect_model.select()
 		self.ArchitectsComboBox.setModel(self.architect_model)
-		self.ArchitectsComboBox.setModelColumn(1)				# Index 1 is arch. name
+		self.ArchitectsComboBox.setModelColumn(1)				# Index 1 is arch name
 
 		self.project_model = QSqlTableModel()
 		self.project_model.setTable("projects")
@@ -52,14 +54,27 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 		self.phase_model.select()
 		self.PhasesComboBox.setModel(self.phase_model)
 		self.PhasesComboBox.setModelColumn(1)					# Index 1 is phase name
+		# set the phases combo box to be the project's current phase - 
+		# 5 is phase_id column number in projects table
+		setCrossComboBox(self.ProjectsComboBox, self.PhasesComboBox, 5)
+		self.ProjectsComboBox.currentIndexChanged.connect(self.projectChanged)
 
 		# enable button clicks
 		self.AddArchitectBtn.clicked.connect(self.addArchitect)
 		self.AddProjectBtn.clicked.connect(self.addProject)
 		self.ViewArchitectsBtn.clicked.connect(self.viewArchitects)
+		self.ViewProjectsBtn.clicked.connect(self.viewProjects)
+		self.LogTimeBtn.clicked.connect(self.logTime)
 
+		# attribute to store the time logger class instance for persistancy
+		self.time_logger = None
 
 		self.show()
+
+	def projectChanged(self) -> None:
+		"""Method to set phase combo box to phase attached to project"""
+		setCrossComboBox(self.ProjectsComboBox, self.PhasesComboBox, 1)
+		self.phase_model.select()
 
 
 	# button methods
@@ -73,7 +88,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 				license_number = arch_window.licenseInput.text(), 
 				phone_number = arch_window.phoneInput.text(),
 				email = arch_window.emailInput.text(), 
-				company_name = arch_window.companyInput.text())
+				company_name = arch_window.companyNameInput.text())
 			with get_db_connection() as conn:
 				cur = conn.cursor()
 				add_architect(new_architect, cur)
@@ -90,10 +105,13 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 			# Get the phase_id from the phases table through the QComboBox model
 			model = proj_window.phaseComboBox.model()
 			phase_id = model.data(model.index(phase_index, 0))
+			# Turn the QDate object into a string
+			start_date = proj_window.projectStartDate.date()
+			start_date_str = start_date.toString("MM-dd-yyyy")
 			new_project = Project(project_name = proj_window.projectNameInput.text(),
 				client_name = proj_window.clientInput.text(),
 				client_address = proj_window.projectAddressInput.text(),
-				start_date = proj_window.projectStartDate.date(),
+				start_date = start_date_str,
 				current_phase_id = phase_id)
 			with get_db_connection() as conn:
 				cur = conn.cursor()
@@ -104,6 +122,16 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 		"""Method to view all architects in database table, click button to
 		include inactive architects"""
 		self.view_arch_window = ViewArchitects()
+
+	def viewProjects(self) -> None:
+		"""Method to view all projects in database table, click button to
+		include completed projects"""
+		self.view_proj_window = ViewProjects(self)
+
+	def logTime(self) -> None:
+		"""Method to activate TimeLogger window and store resulting 
+		TimeEntry object in time_entries database"""
+		self.log_time = TimeLogger(self)
 
 
 		
@@ -152,7 +180,7 @@ class ArchitectWindow(QDialog, Ui_AddArchitectDialog):
 		 
 		# modify phone number to fix (xxx) xxx.xxxx style
 		original_text = self.phoneInput.text()
-		result = phone_pattern.sub(r"(\g<1>) \g<3>.\g<5>", original_text)
+		result = phone_pattern.sub(r"(\g<1>) \g<3>-\g<5>", original_text)
 		self.phoneInput.setText(result)
 
 		super().accept()
@@ -205,9 +233,15 @@ class ViewArchitects(QWidget, Ui_ViewArchitectsWindow):
 		self.setupUi(self)
 		self.setFixedSize(self.size())
 
-		# create query table model and set it on window's TableView
+		# create no-delete table model and set it on window's TableView
 		self.model = NoDeleteTableModel()
 		self.model.setTable("architects")
+
+		# set my view state reset function onto the model
+		self.model.modelReset.connect(self.applyViewState)
+		self.model.layoutChanged.connect(self.applyViewState)
+
+		# set my model on my view
 		self.architectsTableView.setModel(self.model)
 
 		# allow editing of architect information
@@ -220,15 +254,25 @@ class ViewArchitects(QWidget, Ui_ViewArchitectsWindow):
 			"phone_number" : "Phone Number",
 			"email" : "Email Address",
 			"company_name" : "Firm Name",
-			"status" : "Architect Status",
+			"status" : "Status",
 		}
 		for name, title in column_titles.items():
 			index = self.model.fieldIndex(name)
 			self.model.setHeaderData(index, Qt.Horizontal, title)
 
-		# hide architect_id and status columns
+		self.model.select()
+
+		# hide architect_id column
 		self.architectsTableView.setColumnHidden(
 			self.model.fieldIndex("architect_id"), True)
+
+		# set column widths
+		self.architectsTableView.setColumnWidth(1, 150)
+		self.architectsTableView.setColumnWidth(2, 100)
+		self.architectsTableView.setColumnWidth(3, 120)
+		self.architectsTableView.setColumnWidth(4, 200)
+		self.architectsTableView.setColumnWidth(5, 150)
+		self.architectsTableView.setColumnWidth(6, 80)
 
 		# move license number to after company name
 		header = self.architectsTableView.horizontalHeader()
@@ -247,25 +291,193 @@ class ViewArchitects(QWidget, Ui_ViewArchitectsWindow):
 		status_column = self.model.fieldIndex("status")
 		self.architectsTableView.setItemDelegateForColumn(status_column, 
 			status_delegate)
-
-		self.model.select()
 		
-		# set checkbox button to show inactive architects
+		# set checkbox button to hide inactive architects
 		self.hideArchitectCheckBox.stateChanged.connect(self.hideInactiveArchitects)
 		
 		self.show()
 
-	def hideInactiveArchitects(self, signal) -> None:
-		if signal == Qt.Checked:
-			self.model.setFilter("status = 'active'")
-			self.architectsTableView.setColumnHidden(
-				self.model.fieldIndex("status"), True)
+	def hideInactiveArchitects(self, state) -> None:
+		# create filter to hide inactive architects and status column
+		if state == 2:
+			self.model.setFilter("status != 'Inactive'")
 			self.model.select()
+			self.architectsTableView.setColumnWidth(1, 165)
+			self.architectsTableView.setColumnWidth(2, 115)
+			self.architectsTableView.setColumnWidth(3, 135)
+			self.architectsTableView.setColumnWidth(4, 215)
+			self.architectsTableView.setColumnWidth(5, 170)
+	
 		else:
 			self.model.setFilter("")
-			self.architectsTableView.setColumnHidden(
-				self.model.fieldIndex("status"), False)
 			self.model.select()
+			self.architectsTableView.setColumnWidth(1, 150)
+			self.architectsTableView.setColumnWidth(2, 100)
+			self.architectsTableView.setColumnWidth(3, 120)
+			self.architectsTableView.setColumnWidth(4, 200)
+			self.architectsTableView.setColumnWidth(5, 150)
+			self.architectsTableView.setColumnWidth(6, 80)
+
+	def applyViewState(self) -> None:
+		# conditionally hide/show status column based on checkbox
+		status_column = self.model.fieldIndex("status")
+		if status_column != -1:
+			hide_status = self.hideArchitectCheckBox.isChecked()
+			self.architectsTableView.setColumnHidden(status_column, hide_status)
+
+
+class ViewProjects(QWidget, Ui_ViewProjectsWindow):
+	def __init__(self, main_window) -> None:
+		super(ViewProjects, self).__init__()
+		self.main_window = main_window
+		self.setupUi(self)
+		self.setFixedSize(self.size())
+
+		# create a relational table model and set its relation to the phases table
+		self.model = QSqlRelationalTableModel()
+
+		self.projectsTableView.setModel(self.model)
+
+		self.model.setTable("projects")
+		
+		# create a relation between 'projects' table and 'phases' with dropdown menu
+		relation = QSqlRelation("phases", "phase_id", "project_phase")
+		self.model.setRelation(self.model.fieldIndex("current_phase_id"), relation)
+
+		self.projectsTableView.setItemDelegate(QSqlRelationalDelegate(
+			self.projectsTableView))
+
+
+		# set my view state reset function onto the model
+		self.model.modelReset.connect(self.applyViewState)
+		self.model.layoutChanged.connect(self.applyViewState)
+
+		# allow editing of project information
+		self.model.setEditStrategy(QSqlTableModel.OnFieldChange)
+
+		# create and set column headers
+		column_titles = {
+			"project_name" : "Project Name",
+			"client_name" : "Client Name",
+			"client_address" : "Project Address",
+			"start_date" : "Start Date",
+			"current_phase_id" : "Current Phase",
+			"status" : "Status",
+		}
+		for name, title in column_titles.items():
+			index = self.model.fieldIndex(name)
+			self.model.setHeaderData(index, Qt.Horizontal, title)
+
+		self.model.select()
+
+		# hide project_id column
+		self.projectsTableView.setColumnHidden(
+			self.model.fieldIndex("project_id"), True)
+
+		# set column widths
+		self.projectsTableView.setColumnWidth(1, 150)
+		self.projectsTableView.setColumnWidth(2, 160)
+		self.projectsTableView.setColumnWidth(3, 320)
+		self.projectsTableView.setColumnWidth(4, 90)
+		self.projectsTableView.setColumnWidth(5, 180)
+		self.projectsTableView.setColumnWidth(6, 60)
+
+		# set original sort by project name
+		index = self.model.fieldIndex("project_name")
+		self.projectsTableView.sortByColumn(index, Qt.AscendingOrder)
+
+		# allow table to be sorted by clicking top header tabs
+		self.projectsTableView.setSortingEnabled(True)
+
+		# allow only chosen options for status column combo box
+		status_delegate = StatusDelegate(PROJECT_STATUSES, self.projectsTableView)
+		status_column = self.model.fieldIndex("status")
+		self.projectsTableView.setItemDelegateForColumn(status_column, 
+			status_delegate)
+
+		# set checkbox button to show completed projects
+		self.hideProjectCheckBox.stateChanged.connect(self.hideCompletedProjects)
+
+		# set the main window phases combo box when user changes project phase
+		self.model.dataChanged.connect(self.dataChanged)
+
+		self.show()
+
+	def dataChanged(self, topLeft: QModelIndex, bottomRight: QModelIndex, 
+		roles: list = None) -> None:
+		"""Change the phase combo box on the main window when user updates
+		it in the view projects table"""
+		# topLeft.column() will read 5 & 0 instead of just 5 because .dataChanged 
+		# fires twice -> once for the RelationalTableModel and once for the relation 
+		# table. The if only picks up the 2nd which is 0 due to th combo box display 
+		if topLeft.column() == 0:
+			self.main_window.ProjectsComboBox.model().select()
+			setCrossComboBox(self.main_window.ProjectsComboBox, 
+				self.main_window.PhasesComboBox, 5)
+
+	def hideCompletedProjects(self, signal) -> None:
+		"""create filter to hide completed projects and status column"""
+		if signal == 2:
+			self.model.setFilter("status != 'Completed'")
+			self.model.select()
+			self.projectsTableView.setColumnWidth(1, 165)
+			self.projectsTableView.setColumnWidth(2, 175)
+			self.projectsTableView.setColumnWidth(3, 335)
+			self.projectsTableView.setColumnWidth(4, 90)
+			self.projectsTableView.setColumnWidth(5, 195)
+
+		else:
+			self.model.setFilter("")
+			self.model.select()
+			self.projectsTableView.setColumnWidth(1, 150)
+			self.projectsTableView.setColumnWidth(2, 160)
+			self.projectsTableView.setColumnWidth(3, 320)
+			self.projectsTableView.setColumnWidth(4, 90)
+			self.projectsTableView.setColumnWidth(5, 180)
+			self.projectsTableView.setColumnWidth(6, 60)
+
+		self.model.select()
+
+	def applyViewState(self) -> None:
+		# conditionally hide/show status column based on checkbox
+		status_column = self.model.fieldIndex("status")
+		if status_column != -1:
+			hide_status = self.hideProjectCheckBox.isChecked()
+			self.projectsTableView.setColumnHidden(status_column, hide_status)
+
+
+class TimeLogger(QWidget, Ui_TimeLoggerWindow):
+	def __init__(self, main_window) -> None:
+		super(TimeLogger, self).__init__()
+		self.main_window = main_window
+		self.setupUi(self)
+		self.setFixedSize(self.size())
+
+		self.architect_model = QSqlTableModel()
+		self.architect_model.setTable("architects")
+		self.architect_model.select()
+		self.ArchitectComboBox.setModel(self.architect_model)
+		self.ArchitectComboBox.setModelColumn(1)				
+		setLinkedComboBox(self.main_window.ArchitectsComboBox,
+			self.ArchitectComboBox)
+
+		self.project_model = QSqlTableModel()
+		self.project_model.setTable("projects")
+		self.project_model.select()
+		self.ProjectComboBox.setModel(self.project_model)
+		self.ProjectComboBox.setModelColumn(1)					
+		setLinkedComboBox(self.main_window.ArchitectsComboBox,
+			self.ArchitectComboBox)
+		
+		self.phase_model = QSqlTableModel()
+		self.phase_model.setTable("phases")
+		self.phase_model.select()
+		self.PhaseComboBox.setModel(self.phase_model)
+		self.PhaseComboBox.setModelColumn(1)
+		setLinkedComboBox(self.main_window.PhasesComboBox, 
+			self.PhaseComboBox)			
+
+		self.show()
 
 
 class NoDeleteTableModel(QSqlTableModel):
@@ -300,7 +512,20 @@ class StatusDelegate(QStyledItemDelegate):
 		model.setData(index, value)
 
 
+def setCrossComboBox(source_combo: QComboBox, target_combo: QComboBox, 
+	column: int = 1) -> None:
+	"""Function to pull the information from one combo box and populate 
+	it on a combo box elsewhere in the program; column parameter is the column
+	on the source_combo table you want to match to your target_combo's table"""
+	row = source_combo.currentIndex()
+	model = source_combo.model()
+	item_id = model.data(model.index(row, column))
+	matches = target_combo.model().match(target_combo.model().index(0,0),
+		Qt.DisplayRole, item_id)
+	if matches:
+		row = matches[0].row()
+		target_combo.setCurrentIndex(row)
 
-app = QApplication(sys.argv)
-window = MainWindow()
-sys.exit(app.exec())
+def setLinkedComboBox(source_combo: QComboBox, target_combo: QComboBox) -> None:
+	"""Function to set a target ComboBox to the chosen value in the target"""
+	target_combo.setCurrentIndex(source_combo.currentIndex())

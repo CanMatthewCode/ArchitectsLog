617ef145af349fc0de7661f2f9457beeeaddf61c
Matthew Freedman <CanMatthewCode@gmail.com>
2026-02-05 12:12:48 -0800

    add phase_time_entries_by_project function to retrieve all duration_minutes, start_times, and phase_id entries in time_entries table for one project in architectsLog_analytics_db.py

--- src/architectsLog_analytics_db.py
+++ src/architectsLog_analytics_db.py
@@ -2,12 +2,25 @@
 
 import sqlite3
 
-def phase_duration_by_project(project_id: int, cur: sqlite3.Cursor) -> list[tuple[int]]:
-	"""Method to retrieve the summation of a project's phase duration,
-	returns a list of tuples containing (phase_id ints, sum duration ints)"""
+def phase_duration_by_project(project_id: int, 
+	cur: sqlite3.Cursor) -> list[tuple[int, int]]:
+	"""Retrieve the summation of a project's phase duration,
+	returns a list of tuples containing (phase_id, sum duration_minutes)"""
 	sql = "SELECT phase_id, ROUND(SUM(duration_minutes)/60.0, 2) FROM time_entries\
 		WHERE project_id = ? \
 		GROUP BY phase_id \
 		ORDER BY phase_id ASC"
+	cur.execute(sql, (project_id,))
+	return cur.fetchall()
+
+def phase_time_entries_by_project(project_id: int, 
+	cur: sqlite3.Cursor) -> list[tuple[int, int, int]]:
+	"""Retrieve all of a project_id's time_entries duration_minutes with each 
+	corresponding phase_id and start_times. 
+	Returns a list of tuples containing (phase_id, duration_minutes, start_time)"""
+	sql = "SELECT phase_id, duration_minutes, start_time \
+		FROM time_entries \
+		WHERE project_id = ? \
+		ORDER BY start_time ASC"
 	cur.execute(sql, (project_id,))
 	return cur.fetchall()

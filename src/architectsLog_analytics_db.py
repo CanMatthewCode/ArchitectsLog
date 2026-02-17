c45562ff6cde448ab8c7ca4b04be6c4d3b8fe753
Matthew Freedman <CanMatthewCode@gmail.com>
2026-02-17 13:37:21 -0800

    modify phase_duration_by_project_with_name and total_number_of_projects_by_phase functions to accept start and end times 	in architectsLog_analytics_db.py

--- src/architectsLog_analytics_db.py
+++ src/architectsLog_analytics_db.py
@@ -22,18 +22,30 @@ def phase_duration_by_project(project_id: int, cur: sqlite3.Cursor,
 		cur.execute(sql, (project_id,))
 	return cur.fetchall()
 
-def phase_duration_by_project_with_name(project_id: int, 
-	cur: sqlite3.Cursor) -> list[tuple[int, int, str]]:
+def phase_duration_by_project_with_name(project_id: int, cur: sqlite3.Cursor,
+	start_date: Optional[int] = None,
+	end_date: Optional[int] = None) -> list[tuple[int, int, str]]:
 	"""Retrieve the summation of all of a project's per phase duration,
 	returns a list of tuples containing (phase_id, sum duration_minutes, project_name) 
 	per phase"""
-	sql = "SELECT phase_id, ROUND(SUM(duration_minutes)/60.0, 2), projects.project_name \
-		FROM time_entries\
-		INNER JOIN projects ON time_entries.project_id = projects.project_id\
-		WHERE time_entries.project_id = ? \
-		GROUP BY phase_id \
-		ORDER BY phase_id ASC"
-	cur.execute(sql, (project_id,))
+	if start_date and end_date:
+		sql = "SELECT phase_id, ROUND(SUM(duration_minutes)/60.0, 2), \
+			projects.project_name \
+			FROM time_entries\
+			INNER JOIN projects ON time_entries.project_id = projects.project_id\
+			WHERE time_entries.project_id = ? and start_time >= ? and start_time <= ? \
+			GROUP BY phase_id \
+			ORDER BY phase_id ASC"
+		cur.execute(sql, (project_id, start_date, end_date))
+	else:
+		sql = "SELECT phase_id, ROUND(SUM(duration_minutes)/60.0, 2), \
+			projects.project_name \
+			FROM time_entries\
+			INNER JOIN projects ON time_entries.project_id = projects.project_id\
+			WHERE time_entries.project_id = ? \
+			GROUP BY phase_id \
+			ORDER BY phase_id ASC"
+		cur.execute(sql, (project_id,))
 	return cur.fetchall()
 
 def phase_time_entries_by_project(project_id: int, 
@@ -64,15 +76,23 @@ def phase_duration_all_projects( cur: sqlite3.Cursor, start_date: Optional[int] 
 		cur.execute(sql)
 	return cur.fetchall()
 
-def total_number_of_projects_by_phase(cur:sqlite3.Cursor) -> list[tuple[int, int]]:
+def total_number_of_projects_by_phase(cur:sqlite3.Cursor, start_date: Optional[int] = None,
+	end_date: Optional[int] = None) -> list[tuple[int, int]]:
 	"""Retrieve the total number of project_ids per phase in the time_entries table"""
-	cur.execute("SELECT phase_id, COUNT(DISTINCT project_id) FROM time_entries\
-		GROUP BY phase_id ORDER BY phase_id ASC")
+	if start_date and end_date:
+		sql = "SELECT phase_id, COUNT(DISTINCT project_id) FROM time_entries\
+			WHERE start_time >= ? AND start_time <= ? \
+			GROUP BY phase_id ORDER BY phase_id ASC"
+		cur.execute(sql, (start_date, end_date))
+	else:
+		sql = "SELECT phase_id, COUNT(DISTINCT project_id) FROM time_entries\
+			GROUP BY phase_id ORDER BY phase_id ASC"
+		cur.execute(sql)
 	return cur.fetchall()
 
 def project_start_date(project_id: int, cur:sqlite3.Cursor) -> int:
 	"""Retrieve a project_id's start_date as an integer"""
-	sql = "SELECT start_time FROM projects WHERE project_id = ?"
+	sql = "SELECT start_date FROM projects WHERE project_id = ?"
 	cur.execute(sql, (project_id,))
 	return cur.fetchone()
 

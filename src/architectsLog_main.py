bf0c7a3eb58c8f3d78ea5a3ad185ca8858f879b2
Matthew Freedman <CanMatthewCode@gmail.com>
2026-02-23 13:31:10 -0800

    add program_loadup() function in architectsLog_main.py to check if program has been run prior: if not, create ArchitectsLog directory in appropriate OS system directory, 	copy demo_ArchitectsLog.db file to said directory, and create a setting.json file to store last opened database connection in same location; 	if program has been loaded prior, restores last used database from settings.json file and runs program with that database.

--- src/architectsLog_main.py
+++ src/architectsLog_main.py
@@ -1,16 +1,44 @@
 # main entry pathway into the architects log
 
 import sys
+import os
+import shutil
+import json
+from platformdirs import PlatformDirs
 
-from architectsLog_db import DB_FILE, sqltable_initialize
+import architectsLog_db
 from architectsLog_PySide import MainWindow, initialize_database, deleteEmptyInvoices
 
 from PySide6.QtWidgets import QApplication
 
+
+def program_loadup() -> str:
+	dirs = PlatformDirs("ArchitectsLog", ensure_exists=True)
+	settings_path = dirs.user_config_path / "settings.json"
+	data_dir = dirs.user_data_path
+	DB_FILE = ""
+	if not settings_path.exists():
+		log_name = "demo_architectsLog.db"
+		basedir = os.path.dirname(os.path.abspath(__file__))
+		DB_FILE = os.path.join(basedir, log_name)
+		shutil.copy2(DB_FILE, data_dir)
+		settings_dict = {"db_name" : "demo_ArchitectsLog.db"}
+		with open(settings_path, "w") as file:
+			json.dump(settings_dict, file)
+		DB_FILE = os.path.join(data_dir, log_name)
+	else:
+		with open(settings_path) as file:
+			settings = json.load(file)
+		log_name = settings['db_name']
+		DB_FILE = os.path.join(data_dir, log_name)
+	return DB_FILE
+
+
 if __name__ == "__main__":
-	sqltable_initialize()
+	architectsLog_db.DB_FILE = program_loadup()
+	architectsLog_db.sqltable_initialize()
 	app = QApplication()
-	initialize_database(DB_FILE)
+	initialize_database(architectsLog_db.DB_FILE)
 	deleteEmptyInvoices()
 	window = MainWindow()
 	sys.exit(app.exec())
